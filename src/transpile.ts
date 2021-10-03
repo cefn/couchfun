bbae861e948ce59a73ed6ae102632567a41d32d6
Cefn Hoile <github.com@cefn.com>
2021-10-03 21:42:40 +0100

    Preserve only stream implementation

--- src/transpile.ts
+++ src/transpile.ts
@@ -3,40 +3,7 @@ import * as rollup from "rollup";
 import rollupPluginTypescript from "@rollup/plugin-typescript";
 import rollupStream from "@rollup/stream";
 
-export async function transpileSource(
-  sourcePath: string,
-  tsconfigPath: string
-): Promise<void> {
-  const workingDir = process.cwd();
-  const sourceDirRelative = path.dirname(sourcePath);
-  const sourceFileAbsolute = path.resolve(workingDir, sourcePath);
-  const sourceDirAbsolute = path.resolve(workingDir, sourceDirRelative);
-  const input: rollup.InputOptions = {
-    input: sourceFileAbsolute,
-    context: sourceDirAbsolute,
-    plugins: [
-      rollupPluginTypescript({
-        tsconfig: tsconfigPath,
-        target: "es5",
-        sourceMap: false,
-        strict: true,
-        alwaysStrict: false,
-        noImplicitUseStrict: true,
-        downlevelIteration: false,
-        allowSyntheticDefaultImports: false
-      })
-    ],
-    treeshake: false
-  };
-  const output: rollup.OutputOptions = {
-    file: sourceFileAbsolute.replace(/\.ts$/, ".js"),
-    format: "es"
-  };
-  const bundle = await rollup.rollup(input);
-  await bundle.write(output);
-}
-
-export function transpileSourceToStream(
+export function transpileSource(
   sourcePath: string,
   tsconfigPath: string
 ): Promise<string> {
--- test/transpile.test.ts
+++ test/transpile.test.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-import { transpileSource, transpileSourceToStream } from "../src/transpile";
+import { transpileSource } from "../src/transpile";
 
 function deleteFile(filePath: string, ignoreFileMissing = true) {
   try {
@@ -18,51 +18,7 @@ test("transpileSource() generates JS source from TS source", async () => {
   const projectPath = "./test/examples/words/views";
   const sourcePath = `${projectPath}/prefix/map.ts`;
   const tsconfigPath = `${projectPath}/tsconfig.json`;
-  const transpiledPath = sourcePath.replace(/\.ts$/, ".js");
-  try {
-    deleteFile(transpiledPath);
-    await transpileSource(sourcePath, tsconfigPath);
-    expect(
-      fs.existsSync(transpiledPath),
-      `${transpiledPath} not generated as expected`
-    ).toBe(true);
-    const transpiledSource = fs.readFileSync(transpiledPath, "utf-8");
-    expect(transpiledSource).toMatchInlineSnapshot(`
-      "function docIsWord(doc) {
-          return doc.type === \\"word\\";
-      }
-
-      function eachPrefix(word, fn) {
-          var spelling = word.id;
-          var length = spelling.length;
-          if (length > 0) {
-              for (var last = 1; last <= length; last++) {
-                  fn(spelling.substr(0, last));
-              }
-          }
-      }
-
-      function emitPrefixes(doc) {
-          if (docIsWord(doc)) {
-              eachPrefix(doc, emit);
-          }
-      }
-      emitPrefixes;
-      "
-    `);
-  } finally {
-    deleteFile(transpiledPath);
-  }
-});
-
-test("transpileSourceToStream() generates JS source from TS source", async () => {
-  const projectPath = "./test/examples/words/views";
-  const sourcePath = `${projectPath}/prefix/map.ts`;
-  const tsconfigPath = `${projectPath}/tsconfig.json`;
-  const transpiledSource = await transpileSourceToStream(
-    sourcePath,
-    tsconfigPath
-  );
+  const transpiledSource = await transpileSource(sourcePath, tsconfigPath);
   expect(transpiledSource).toMatchInlineSnapshot(`
       "function docIsWord(doc) {
           return doc.type === \\"word\\";
