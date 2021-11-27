f3b5be12462d660ac410202edd7032c95428bebe
Cefn Hoile <github.com@cefn.com>
2021-11-27 21:45:01 +0000

    Define word count map-reduce

--- test/examples/nobel/views/cloud/map.ts
+++ test/examples/nobel/views/cloud/map.ts
@@ -4,8 +4,10 @@ import { Prize, PrizeAwarded } from "../../types";
 function mapMotivationTerms(prize: Prize) {
   const { overallMotivation } = prize;
   if (overallMotivation) {
-    for (const word of overallMotivation.split(" ")) {
-      emit(word);
+    for (const word of overallMotivation.split(/[^A-Za-z]/)) {
+      if (word) {
+        emit(word);
+      }
     }
   }
 
@@ -15,9 +17,11 @@ function mapMotivationTerms(prize: Prize) {
     for (const laureate of laureates) {
       const { motivation } = laureate;
       // skip duplicate motivations
-      if (motivation !== lastMotivation) {
-        for (const word of motivation.split(" ")) {
-          emit(word);
+      if (motivation && motivation !== lastMotivation) {
+        for (const word of motivation.split(/[^A-Za-z]/)) {
+          if (word) {
+            emit(word);
+          }
         }
       }
     }
